f997c9c113504a26710b8bd8252a804c76b3dcae
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2023-12-17 11:38:59 +0000

    Assignment2 task1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { LEADERS } from '../shared/leaders';
 import Home from "./HomeComponent";
 import {Switch,Route,Redirect} from "react-router-dom";
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -20,16 +21,19 @@ class Main extends Component {
         dishes: DISHES,
         comments: COMMENTS,
       promotions: PROMOTIONS,
-      leaders: LEADERS,
-        selectedDish: null
+      leaders: LEADERS
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+ 
 
   render() {
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     const HomePage = () => {
       return(
         <Home 
@@ -45,7 +49,9 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} /> } />
               <Redirect to="/home" />
           </Switch>
         <Footer />
